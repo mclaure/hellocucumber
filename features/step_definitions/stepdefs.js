3bdef2c46ad3edc9b9791718ba365b825b5afe71
Marcelo Claure <marcelo.claure@niceincontact.com>
2019-10-17 17:32:21 -0400

    cucumber example

---
+++ features/step_definitions/stepdefs.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { Given, When, Then } = require('cucumber');
+
+function isItFriday(today) {
+    return (today == 'Friday') ? 'TGIF' : 'Nope';
+  }
+
+Given('today is {string}', function (givenDay) {
+    this.today = givenDay;
+});
+
+When('I ask whether it\'s Friday yet', function () {
+    this.actualAnswer = isItFriday(this.today);
+});
+
+Then('I should be told {string}', function (expectedAnswer) {
+    assert.equal(this.actualAnswer, expectedAnswer);
+});
